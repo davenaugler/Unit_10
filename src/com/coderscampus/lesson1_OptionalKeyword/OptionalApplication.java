bb5a76d40a7948dd77086ab9c151ff6b6b558c9d
Dave Naugler <davenaugler@gmail.com>
2023-08-12 16:17:53 -0600

    Saturday afternoon commit

--- src/com/coderscampus/lesson1_OptionalKeyword/OptionalApplication.java
+++ src/com/coderscampus/lesson1_OptionalKeyword/OptionalApplication.java
@@ -1,9 +1,33 @@
 package com.coderscampus.lesson1_OptionalKeyword;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 public class OptionalApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Lesson 1
+		Optional<String> fullName = Optional.of("Dave Naugler");
+		
+		System.out.println(fullName);
+		
+		System.out.println(fullName.get());
+		
+		String dave = fullName.get();
+		System.out.println(dave);
+		
+		// Lesson 2
+		Recipe brownies = new Recipe(); 
+		System.out.println("---------");
+		brownies.setName("Chocolate Chunk Brownies");
+		brownies.setInstructions("1. Pre-heat oven\n2. Mix dry ingredients\n3. Mix wet ingredients\n4. Mix all ingredients\n5. Put in oven for 20 minutes");
+//		System.out.println(brownies.getName());
+//		System.out.println(brownies.getInstructions());
+//		System.out.println(brownies);
+		
+		// Lesson 3
+		brownies.setPriceOpt((new BigDecimal(9.99)));
+		System.out.println(brownies);
 
 	}
 
---
+++ src/com/coderscampus/lesson1_OptionalKeyword/Recipe.java
@@ -0,0 +1,58 @@
+package com.coderscampus.lesson1_OptionalKeyword;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+public class Recipe {
+
+	private String name;
+	private String instructions;
+//	private Optional<BigDecimal> priceOpt; // Adding "Opt" at the end of the variable name price, indicates it's
+											// Optional
+	private Optional<BigDecimal> priceOpt = Optional.empty(); // If you need to assigning null to an Optional, this is how you do it
+	// rather than assigning 'null' to it.
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getInstructions() {
+		return instructions;
+	}
+
+	public void setInstructions(String instructions) {
+		this.instructions = instructions;
+	}
+
+	public Optional<BigDecimal> getPriceOpt() {
+		return priceOpt;
+	} 
+
+	// When setting an Optional you do not need the "Optional" data type wrapper around BigDecimal
+	// When using Optional the point is to get away from using null. Don't put "null" where "price" is on line 38
+	public void setPriceOpt(BigDecimal price) {
+		this.priceOpt = Optional.of(price); 
+		// will return a "NullPointerException"
+//		this.priceOpt = Optional.ofNullable(null);
+	}
+
+	@Override
+	public String toString() {
+		return "Recipe [name=" + name + ", instructions=" + instructions + getPrice() + "]";
+	}
+
+	private String getPrice() {
+		String priceString = priceOpt.map(price -> ", price=" + price)
+									 .orElse(", price=free");
+		return priceString;
+	}
+	
+	
+	
+	
+
+}
